045c501d2775527c78e8a848ece1985f59ee3efe
hyukzz <ri_chard@naver.com>
2023-05-09 22:02:15 +0900

    feat: pokemons page

---
+++ src/app/components/Pokemons.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { getPokemons } from '../store';
+import PokemonCard from './PokemonCard';
+import Link from 'next/link';
+
+const Pokemons = () => {
+  const { data } = useQuery({
+    queryKey: ['pokemons'],
+    queryFn: () => getPokemons(),
+    staleTime: 1000 * 60,
+  });
+  return (
+    <div className='container mx-auto my-16'>
+      <ul className='flex flex-col gap-4 items-center'>
+        {data?.results?.map(({ name, imgUrl, id }: { name: string; imgUrl: string; id: number }) => (
+          <li key={id}>
+            <Link href={`/pokemon/${id}`}>
+              <PokemonCard id={id} name={name} imgUrl={imgUrl} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Pokemons;
